22f8acd92dfd47376bdca2def6cb5f73790cecdf
aishaadedire <110497802+aishaadedire@users.noreply.github.com>
2023-09-09 01:53:11 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const currentDayText = document.querySelector("#currentDayOfTheWeek");
+const currentUTCTime = document.getElementById("time");
+const date = new Date();
+
+const generateCurrentDate = () => {
+  const daysOfTheWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const dayIndex = date.getDay();
+  const currentDay = daysOfTheWeek[dayIndex];
+  currentDayText.innerText = currentDay;
+};
+
+const generateUTCTime = () => {
+  const date = new Date();
+  const utcTimeMilliseconds = date.getTime();
+  currentUTCTime.textContent = utcTimeMilliseconds;
+};
+
+generateCurrentDate();
+generateUTCTime();
+setInterval(generateUTCTime, 1000);
